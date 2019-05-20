6edf06d5a833cb18da99c612cfcfc51e4b3c9efa
rikisaraan <rikisaraan2@gmail.com>
2019-05-20 20:20:58 +0700

    update ckeditor with wiris example

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Tes Wiris</title>
+
+        <script src="ckeditor/plugins/ckeditor_wiris/integration/WIRISplugins.js?viewer=image"></script>
+        <script src="ckeditor/ckeditor.js"></script>
+    </head>
+    <body>
+        <textarea class="ckeditor" name="editor1" id="editor1" rows="10" cols="80">
+                This is my textarea to be replaced with CKEditor.
+        </textarea>
+    </body>
+</html>
